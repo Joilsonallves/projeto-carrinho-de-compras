8f1b63332824604f954daf65ad1167e0f5ad92fa
Joilsonallves <130906367+Joilsonallves@users.noreply.github.com>
2025-03-15 10:22:52 -0300

    Update

--- js/script.js
+++ js/script.js
@@ -336,7 +336,7 @@ async function enviarEmail() {
         message: 'Sua compra foi finalizada com sucesso!'
     };
 
-    emailjs.send('service_12345', 'template_12345', templateParams) // Substitua pelo seu Service ID e Template ID
+    emailjs.send('service_hqb1eid', 'template_2cvpckf', templateParams) // Substitua pelo seu Service ID e Template ID
         .then(response => {
             console.log('E-mail enviado com sucesso!', response.status, response.text);
             alert('Notificação enviada por e-mail!');
